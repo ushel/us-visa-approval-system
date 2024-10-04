01c6adc4675eef98b5799f877474f944414331e8
ushel <utkarsh.shelke03@gmail.com>
2024-10-04 19:48:03 +0530

    data ingestion complete

--- demo.py
+++ demo.py
@@ -1,11 +1,21 @@
-import sys
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
+# import sys
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
 
-logging.info("Welcome to our custom log")
+# logging.info("Welcome to our custom log")
 
-try:
-    a = 1/"10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e,sys) from e
+# try:
+#     a = 1/"10"
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e,sys) from e
+
+
+# from us_visa.constants import DATABASE_NAME
+# print(DATABASE_NAME, "Welcome")
+
+from us_visa.constants import *   #To import all the constants from the file...
+
+print(DATABASE_NAME, "Welcome")
+
+print(COLLECTION_NAME, "Welcome")
