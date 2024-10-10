553814c05b60c781595e64a3c5b0fa5a9b3f1b55
ushel <utkarsh.shelke03@gmail.com>
2024-10-10 21:10:06 +0530

    Data validation started

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -29,3 +29,12 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_score"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation constant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "data_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
